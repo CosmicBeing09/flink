589c3608c5a5c07f3ebd25b05aa28518ad0ada8c
Raihan Ullah <raihan-iict@sust.edu>
2025-07-15 21:23:43 -0600

    Rename createLatencyTrackingState to createMetricsTrackingState in flink-runtime/src/test/java/org/apache/flink/runtime/state/metrics/LatencyTrackingStateTestBase.java

--- flink-runtime/src/test/java/org/apache/flink/runtime/state/metrics/LatencyTrackingStateTestBase.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/state/metrics/LatencyTrackingStateTestBase.java
@@ -1,22 +1,22 @@
 /*
 
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements.  See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership.  The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License.  You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
 
-*/
+ */
 
 package org.apache.flink.runtime.state.metrics;
 
@@ -86,15 +86,15 @@ abstract class LatencyTrackingStateTestBase<K> {
 
     @SuppressWarnings("unchecked")
     protected <
-                    N,
-                    V,
-                    S extends InternalKvState<K, N, V>,
-                    S2 extends State,
-                    LSM extends StateLatencyMetricBase>
-            AbstractLatencyTrackState<K, N, V, S, LSM> createLatencyTrackingState(
-                    AbstractKeyedStateBackend<K> keyedBackend,
-                    StateDescriptor<S2, V> stateDescriptor)
-                    throws Exception {
+            N,
+            V,
+            S extends InternalKvState<K, N, V>,
+            S2 extends State,
+            LSM extends StateLatencyMetricBase>
+    AbstractLatencyTrackState<K, N, V, S, LSM> createMetricsTrackingState(
+            AbstractKeyedStateBackend<K> keyedBackend,
+            StateDescriptor<S2, V> stateDescriptor)
+            throws Exception {
 
         S2 keyedState =
                 keyedBackend.getOrCreateKeyedState(
@@ -115,7 +115,7 @@ abstract class LatencyTrackingStateTestBase<K> {
         AbstractKeyedStateBackend<K> keyedBackend = createKeyedBackend(getKeySerializer());
         try {
             AbstractLatencyTrackState latencyTrackingState =
-                    createLatencyTrackingState(keyedBackend, getStateDescriptor());
+                    createMetricsTrackingState(keyedBackend, getStateDescriptor());
             latencyTrackingState.setCurrentNamespace(VoidNamespace.INSTANCE);
             StateLatencyMetricBase latencyTrackingStateMetric =
                     latencyTrackingState.getLatencyTrackingStateMetric();
