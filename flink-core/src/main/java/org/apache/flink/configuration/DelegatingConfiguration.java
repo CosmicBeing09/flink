bc929536c8e0c0982acc357eac1f36b2c2b6e9dc
Raihan Ullah <raihan-iict@sust.edu>
2025-07-17 17:33:20 -0600

    commit changes for analysis

--- flink-core/src/main/java/org/apache/flink/configuration/DelegatingConfiguration.java
+++ flink-core/src/main/java/org/apache/flink/configuration/DelegatingConfiguration.java
@@ -103,8 +103,8 @@ public final class DelegatingConfiguration extends Configuration {
     @Override
     public void addAllToProperties(Properties props) {
         // only add keys with our prefix
-        synchronized (backingConfig.confData) {
-            for (Map.Entry<String, Object> entry : backingConfig.confData.entrySet()) {
+        synchronized (backingConfig.configData) {
+            for (Map.Entry<String, Object> entry : backingConfig.configData.entrySet()) {
                 if (entry.getKey().startsWith(prefix)) {
                     String keyWithoutPrefix =
                             entry.getKey().substring(prefix.length(), entry.getKey().length());
