171294eb39d358c5fbc5cd046bb427fd4daf732d
Raihan Ullah <raihan-iict@sust.edu>
2025-07-18 01:07:12 -0600

    commit changes for analysis

--- flink-core/src/main/java/org/apache/flink/core/fs/FSDataInputStream.java
+++ flink-core/src/main/java/org/apache/flink/core/fs/FSDataInputStream.java
@@ -36,10 +36,10 @@ public abstract class FSDataInputStream extends InputStream {
      * Seek to the given offset from the start of the file. The next read() will be from that
      * location. Can't seek past the end of the stream.
      *
-     * @param desired the desired offset
+     * @param offset the desired offset
      * @throws IOException Thrown if an error occurred while seeking inside the input stream.
      */
-    public abstract void seek(long desired) throws IOException;
+    public abstract void seek(long offset) throws IOException;
 
     /**
      * Gets the current position in the input stream.
