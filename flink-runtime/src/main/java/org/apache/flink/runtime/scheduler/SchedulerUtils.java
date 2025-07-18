1433949de3e44f1caf61e604031c6270ac3b45f2
Raihan Ullah <raihan-iict@sust.edu>
2025-07-17 22:59:39 -0600

    commit changes for analysis

--- flink-runtime/src/main/java/org/apache/flink/runtime/scheduler/SchedulerUtils.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/scheduler/SchedulerUtils.java
@@ -50,19 +50,19 @@ public final class SchedulerUtils {
 
     public static CompletedCheckpointStore createCompletedCheckpointStoreIfCheckpointingIsEnabled(
             JobGraph jobGraph,
-            Configuration configuration,
+            Configuration jobManagerConfiguration,
             CheckpointRecoveryFactory checkpointRecoveryFactory,
             Executor ioExecutor,
-            Logger log)
+            Logger logger)
             throws JobExecutionException {
         final JobID jobId = jobGraph.getJobID();
         if (DefaultExecutionGraphBuilder.isCheckpointingEnabled(jobGraph)) {
             try {
                 return createCompletedCheckpointStore(
-                        configuration,
+                        jobManagerConfiguration,
                         checkpointRecoveryFactory,
                         ioExecutor,
-                        log,
+                        logger,
                         jobId,
                         jobGraph.getSavepointRestoreSettings().getRecoveryClaimMode());
             } catch (Exception e) {
